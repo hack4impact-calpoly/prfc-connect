78fc3060fb3b4fe8e6bebd65a7182b95e9e35af0
Kevin Rutledge <kevin.rutledge.89@gmail.com>
2026-04-11 23:02:12 -0700

    refactor: convert next.config to typescript esm

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
@@ -34,4 +35,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
